db6acc30e9bdba9ebc44cc063237ebfe0adab257
isabelkay <163697326+isabelkay@users.noreply.github.com>
2026-03-06 19:38:33 -0500

    xx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -96,6 +96,7 @@ export default function Home() {
       }
 
     closeModal();
+    fetchFiles(); 
     window.dispatchEvent(new Event("filesUpdated"));
     } catch (err) {
       console.error("Error adding file:", err);
